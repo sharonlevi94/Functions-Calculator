615e70addb2777fafaef8fc099af803b31e80359
sharonlevi94 <sharonlevy96@gmail.com>
2021-03-21 11:45:23 +0200

    added enum and map commands in the main, still working on it

---
+++ include/Macros.h
@@ -0,0 +1,7 @@
+#pragma once
+constexpr auto EVAL = "eval";
+constexpr auto POLY = "poly";
+constexpr auto MUL = "mul";
+constexpr auto ADD = "add";
+constexpr auto COMP = "comp";
+constexpr auto LOG = "log";
--- src/ex1_sharon_levi_bar_ifrah.cpp
+++ src/ex1_sharon_levi_bar_ifrah.cpp
@@ -1,10 +1,39 @@
-﻿#include <iostream>
+﻿//include section:
+#include <iostream>
 #include "Poly.h"
+#include "Menu.h"
+#include <string>
+#include <map>
+#include "Macros.h"
+//using section:
 using std::vector;
-
+using std::string;
+using std::map;
+//enum section:
+enum Commands { eval=0, poly, mul, add, comp, log };
+//prototypes section:
+void setCommandsList(map<string, Commands>);
+//main section:
 int main() {
-    vector<double> factors{ 1,1,1 };
-    Poly p(2, factors);
-    std::cout << p << std::endl;
-    return 0;
+    Menu list;
+    map<string, Commands> command_list;
+    while (true) {
+        list.showMenu();
+        string command,args;
+        setCommandsList(command_list);
+        std::cin >> command >> args;
+
+    }
+    
+    return EXIT_SUCCESS;
+}
+
+//functions section:
+void setCommandsList(map<string,Commands> commands) {
+    commands.insert(std::pair<string, Commands>(EVAL, eval));
+    commands.insert(std::pair<string, Commands>(POLY, poly));
+    commands.insert(std::pair<string, Commands>(MUL, mul));
+    commands.insert(std::pair<string, Commands>(ADD, add));
+    commands.insert(std::pair<string, Commands>(COMP, comp));
+    commands.insert(std::pair<string, Commands>(LOG, log));
 }
